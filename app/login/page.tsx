b7a9c063c1bb0492823c5c0f91e874c593eaff50
prachee-en <prachiagarwal2002july@gmail.com>
2024-04-26 00:44:39 +0530

    made frontend of draupadi on dias

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,7 @@ export default function Login(){
   <div className="hero-content flex-col lg:flex-row-reverse">
     <div className="text-center lg:text-left">
       <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <p className="py-6">If you are a speaker, panellist, thought leader or knowledge expert in your field, sign up to help event organisers find and invite you to leadership-level panels and conferences.</p>
     </div>
     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
       <form className="card-body">
